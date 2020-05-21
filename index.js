db0410894df49a8895542b96a634f22dfcbd61cf
mattjohannsen <majohann@hotmail.com>
2020-05-20 20:52:34 -0500

    Completed Javascript Whiteboard challenges #1

--- index.js
+++ index.js
@@ -3,3 +3,25 @@
 function HelloWorld(){
     alert("Hello World!");
 }
+
+function Problem1(){
+  var inputArray = [5, 17, 77, 50];
+  var target = 55;
+  var indexNum1;
+  var indexNum2;
+  for (let i = 0; i < inputArray.length; i++) {
+      console.log(inputArray[i]);
+      for (let j = 0; j < inputArray.length; j++) {
+        console.log(inputArray[j]);
+          if (i === j) {
+            continue;
+          } else {
+            if(inputArray[i]+inputArray[j]===target){
+              indexNum1=i;
+              indexNum2=j;
+            }
+          }
+      }
+  }
+  alert("Indeces: " + indexNum1 + ", " + indexNum2);
+}
