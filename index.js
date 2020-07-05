99b3941291f7a595d38578028bab49c571272fe4
mattjohannsen <majohann@hotmail.com>
2020-07-05 15:08:35 -0500

    Completed Problem 29: Return the Sum of the Two Smallest Numbers. Total time to solve 11 minutes

--- index.js
+++ index.js
@@ -709,6 +709,48 @@ function isPrime(inputNumber){
   }
   return isNumberPrime;
 }
+function Problem29(){
+  var originalArray = [19, 5, 42, 2, 77];
+  var theAnswer = SumSmallest(originalArray);
+  alert(theAnswer);
+  originalArray = [10, 343445353, 3453445, 3453545353453];
+  theAnswer = SumSmallest(originalArray);
+  alert(theAnswer);
+  originalArray = [2, 9, 6, -1];
+  theAnswer = SumSmallest(originalArray);
+  alert(theAnswer);
+  originalArray = [879, 953, 694, -847, 342, 221, -91, -723, 791, -587];
+  theAnswer = SumSmallest(originalArray);
+  alert(theAnswer);
+  originalArray = [3683, 2902, 3951, -475, 1617, -2385];
+  theAnswer = SumSmallest(originalArray);
+  alert(theAnswer);
+}
+function SumSmallest(inputArray){
+  var sortedArray = inputArray.sort((a,b) => a-b);
+  var startingPoint = GetStartingPoint(sortedArray);
+  return (sortedArray[startingPoint] + sortedArray[startingPoint+1]);
+  
+}
+function GetStartingPoint(inputArray){
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i]>0) {
+      return i;
+    }
+  }
+}
+//Return the Sum of the Two Smallest Numbers
+//Create a function that takes an array of numbers and returns the sum of the two lowest positive numbers.
+//Examples
+//SumSmallest([19, 5, 42, 2, 77]) --> 7
+//SumSmallest([10, 343445353, 3453445, 3453545353453]) --> 3453455
+//SumSmallest([2, 9, 6, -1]) --> 8
+//SumSmallest([879, 953, 694, -847, 342, 221, -91, -723, 791, -587]) --> 563
+//SumSmallest([3683, 2902, 3951, -475, 1617, -2385]) --> 4519
+//Notes
+//Don't count negative numbers.
+//Floats and empty arrays will not be used in any of the test cases.
+
 
 // Check if a Number is Prime https://edabit.com/challenge/aoR4PFS6FfpJs6v79
 // Create a function that outputs true if a number is prime, and false otherwise.
