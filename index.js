b2584933d7cdc87de3c96e9fa823bf74cb2d1a59
mattjohannsen <majohann@hotmail.com>
2020-08-09 11:39:00 -0500

    Completed Challenge 52: Convert Zero & One to 1 and 0. Total time to solve 19 minutes

--- index.js
+++ index.js
@@ -1260,6 +1260,31 @@ function Problem51(){
 function CommentsCorrect(str){
   return (str.replace(/\/\*\*\//g, '').replace(/\/\//g, '') == "");
 }
+function Problem52(){
+  alert(TextToNumberBinary("zero one zero one zero one zero one"));
+  alert(TextToNumberBinary("Zero one zero ONE zero one zero one"));
+  alert(TextToNumberBinary("zero one zero one zero one zero one one two"));
+  alert(TextToNumberBinary("zero one zero one zero one zero three"));
+  alert(TextToNumberBinary("one one"));
+}
+function TextToNumberBinary(str){
+  str = str.toLowerCase().replace(/one/g, "1");
+  str = str.toLowerCase().replace(/zero/g, "0");
+  str = str.replace(/[^0-1]/g, "");
+  var stringLength = str.length - (str.length % 8);
+  var returnString = str.substring(0, stringLength);
+  return returnString;
+}
+//Convert "Zero" and "One" to "1" and "0"
+//Create a function that takes a string as an argument.The function must return a string containing 1s and 0s based on the string argument's words. If any word in the argument is not equal to "zero" or "one" (case insensitive), you should ignore it. The returned string's length should be a multiple of 8, if the string is not a multiple of 8 you should remove the numbers in excess.
+//Examples
+//TextToNumberBinary("zero one zero one zero one zero one") --> "01010101"
+//TextToNumberBinary("Zero one zero ONE zero one zero one") --> "01010101"
+//TextToNumberBinary("zero one zero one zero one zero one one two") --> "01010101"
+//TextToNumberBinary("zero one zero one zero one zero three") --> ""
+//TextToNumberBinary("one one") --> ""
+//Notes
+
 //Valid JavaScript Comments
 //In JavaScript, there are two types of comments:
 //Single-line comments start with //
