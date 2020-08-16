e27df48aabf300e9879afe34d3d9983b8ef6e4fa
mattjohannsen <majohann@hotmail.com>
2020-08-16 13:54:29 -0500

    Completed Challenge 60: Briefcase Lock

--- index.js
+++ index.js
@@ -1387,6 +1387,45 @@ function RollingCipher(str, n){
   }
   return arr.join('');
 }
+function Problem60(){
+  alert(MinTurns("4089", "5672"));
+  alert(MinTurns("1111", "1100"));
+  alert(MinTurns("2391", "4984"));
+}
+function MinTurns(current, target){
+  var total = 0;
+  for (let i = 0; i < 4; i++) {
+    var largest = Math.max(current[i], target[i]);
+    var smallest = Math.min(current[i], target[i]);
+    total += Math.min(largest-smallest, Math.abs(largest-(smallest + 10)));
+  }
+  return total;
+}
+// Briefcase Lock
+// A briefcase has a 4-digit rolling-lock. Each digit is a number from 0-9 that can be rolled either forwards or backwards.
+// Create a function that returns the smallest number of turns it takes to transform the lock from the current combination to the target combination. One turn is equivalent to rolling a number forwards or backwards by one.
+// To illustrate:
+// current-lock: 4089
+// target-lock: 5672
+// What is the minimum number of turns it takes to transform 4089 to 5672?
+// 4 --> 5
+// 4 --> 5  // Forward Turns: 1 <- Min
+// 4 --> 3 --> 2 --> 1 --> 0 --> 9 --> 8 --> 7 --> 6 --> 5  // Backward Turns: 9
+// 0 --> 6
+// 0 --> 1 --> 2 --> 3 --> 4 --> 5 --> 6  // Forward Turns: 6
+// 0 --> 9 --> 8 --> 7 --> 6  // Backward Turns: 4  <- Min
+// 8 --> 7
+// 8 --> 9 --> 0 --> 1 --> 2 --> 3 --> 4 --> 5 --> 6 --> 7  // Forward Turns: 9
+// 8 --> 7  // Backward Turns: 1  <- Min
+// 9 --> 2
+// 9 --> 0 --> 1 --> 2  // Forward Turns: 3  <- Min
+// 9 --> 8 --> 7 --> 6 --> 5 --> 4 --> 3 --> 2  // Backward Turns: 7
+// It takes 1 + 4 + 1 + 3 = 9 minimum turns to change the lock from 4089 to 5672.
+// Examples
+// MinTurns(4089, 5672) --> 9
+// MinTurns(1111, 1100) --> 2
+// MinTurns(2391, 4984) --> 10
+
 //Rolling Cipher
 //Write a function that accepts a string and an n and returns a cipher by rolling each character forward(n > 0) or backward(n< 0) n times.
 //Note: Think of the letters as a connected loop, so rolling a backwards once will yield z, and rolling z forward once will yield a. If you roll 26 times in either direction, you should end up back where you started.
