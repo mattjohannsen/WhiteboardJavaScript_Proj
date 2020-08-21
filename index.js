bc330e792b0c08522cbe0819f16dea6aa9766730
mattjohannsen <majohann@hotmail.com>
2020-08-20 19:58:02 -0500

    Completed Challenge 63: Break Point

--- index.js
+++ index.js
@@ -1445,6 +1445,40 @@ function Simplify(str){
   while (fractionIsNotSimplified);
   return (arr[0] % arr[1] == 0) ? arr[0] / arr[1] : arr.join("/");
 }
+function Problem63(){
+  alert(BreakPoint(159780));
+  alert(BreakPoint(112));
+  alert(BreakPoint(1034));
+  alert(BreakPoint(10));
+  alert(BreakPoint(343));
+}
+function BreakPoint(num){
+  var arr = num.toString(10).split('').map(Number);
+  let leftSum = 0;
+  let rightSum = arr.reduce((a, b) => a + b, 0);
+  for (let i = 0; i < arr.length; i++) {
+    leftSum += arr[i];
+    rightSum -= arr[i];
+    if (leftSum == rightSum) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//Break Point
+//A number has a breakpoint if it can be split in a way where the digits on the left side and the digits on the right side sum to the same number.
+//For instance, the number 35190 can be sliced between the digits 351 and 90, since 3 + 5 + 1 = 9 and 9 + 0 = 9. On the other hand, the number 555 does not have a breakpoint(you must split between digits).
+//Create a function that returns true if a number has a breakpoint, and false otherwise.
+//Examples
+//BreakPoint(159780) --> true
+//BreakPoint(112) --> true
+//BreakPoint(1034) --> true
+//BreakPoint(10) --> false
+//BreakPoint(343)  --> false
+//Notes
+//Read each digit as only one number.
+
 // Simplified Fractions
 // Create a function that returns the simplified version of a fraction.
 
