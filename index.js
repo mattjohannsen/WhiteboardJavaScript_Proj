1fba30997442d04143d498db9d4eaeeb544ec4a7
mattjohannsen <majohann@hotmail.com>
2020-08-23 12:21:45 -0500

    Completed Challenge 66: First Index

--- index.js
+++ index.js
@@ -1543,6 +1543,39 @@ function EncodeMorse(str){
   }
   return morseCode.join(' ');
 }
+function Problem66(){
+  alert(FirstIndex("68 65 6c 6c 6f 20 77 6f 72 6c 64", "world"));
+  alert(FirstIndex("47 6f 6f 64 62 79 65 20 77 6f 72 6c 64", "world"));
+  alert(FirstIndex("42 6f 72 65 64 20 77 6f 72 6c 64", "Bored"));
+}
+function FirstIndex(hexString, needle){
+  var arr = hexString.split(' ');
+  var theNeedle = needle.charCodeAt(0).toString(16);
+  return arr.indexOf(theNeedle)
+}
+
+function Problem38(){
+  alert(ConvertToHex("hello world"));
+  alert(ConvertToHex("Big Boi"));
+  alert(ConvertToHex("Marty Poppinson"));
+}
+function ConvertToHex(inputString){
+  var stringAsArray = inputString.split('');
+  var hexArray = [];
+  for (let i = 0; i < stringAsArray.length; i++) {
+    hexArray[i] = stringAsArray[i].charCodeAt(0).toString(16);
+  }
+  return hexArray.join(' ');
+}
+
+//Needle in a Hex String
+//Find the index of a string within a hex encoded string.
+//You will be given a string which needs to be found in another string which has previously been translated into hex.You will need to return the first index of the needle within the hex encoded string.
+//Examples
+//FirstIndex("68 65 6c 6c 6f 20 77 6f 72 6c 64", "world") --> 6
+//FirstIndex("47 6f 6f 64 62 79 65 20 77 6f 72 6c 64", "world") --> 8
+//FirstIndex("42 6f 72 65 64 20 77 6f 72 6c 64", "Bored") --> 0
+
 //Encode Morse
 //Create a function that takes a string as an argument and return a non-encoded, encrypted string.
 //Examples
